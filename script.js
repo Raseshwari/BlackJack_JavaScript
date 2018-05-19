366660fbcb929f5a9a330aca851536c86b9d41f7
Raseshwari <32209693+Raseshwari@users.noreply.github.com>
2018-05-19 11:52:14 -0400

    updated to include card shuffle and game over logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
   BlackJack Implementation
 */
 
+//Card Variables
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 
 let values = ['Ace', 'King', 'Queen', 'Jack',
@@ -9,6 +10,43 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
   'Five', 'Four', 'Three', 'Two'
   ];
 
+//DOM variables
+let textArea = document.getElementById("para");
+let newGameButton = document.getElementById("new-game-button");
+let hitButton = document.getElementById("hit-button");
+let stayButton = document.getElementById("stay-button");
+
+//Game variables
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    cardDeck = [];
+    
+
+hitButton.style.display = "none";
+stayButton.style.display = "none";
+showStatus();
+
+newGameButton.addEventListener('click',function(){
+  
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+  
+  cardDeck = createCardDeck();
+  shuffleCardDeck(cardDeck);
+  playerCards = [getNextCard(), getNextCard()];
+  dealerCards = [getNextCard(), getNextCard()];
+  
+  newGameButton.style.display = "none";
+  hitButton.style.display = "inline";
+  stayButton.style.display = "inline";
+  showStatus();
+});
 
 
 function createCardDeck()
@@ -40,15 +78,66 @@ function getNextCard()
   return cardDeck.shift();
 }
 
-let cardDeck = createCardDeck();
+function shuffleCardDeck(cardDeck)
+{
+  for(let i = 0; i<cardDeck.length; i++)
+  {
+    let swapIndx = Math.trunc(Math.random()*cardDeck.length);
+    let temp = cardDeck[swapIndx];
+    cardDeck[swapIndx] = cardDeck[i];
+    cardDeck[i] = temp;
+  }
+}
 
-let playerCards = [getNextCard(), getNextCard()]
 
-console.log("Welcome to BlackJack");
+function showStatus()
+{
+  if(!gameStarted)
+  {
+    textArea.innerText = "Welcome to BlackJack!!"
+  }else
+  {
+    textArea.innerText = "Game Started.."
+  }
+  
+  let dealerCardString = '';
+  for(let i = 0; i<dealerCards.length; i++)
+  {
+    dealerCardString +=getCardAsString(dealerCards[i])+"\n";
+  }
+  
+  let playerCardString = '';
+  for(let i = 0; i<playerCards.length; i++)
+  {
+    playerCardString +=getCardAsString(playerCards[i])+"\n";
+  }
+  
+  updateScores();
+  
+  textArea.innerText = 
+  "Dealer has:"+"\n"+
+  dealerCardString+"\n Score:"+dealerScore+
+  "\n\n"+
+  "Player has:"+"\n"+
+  playerCardString+"\n Score:"+playerScore+
+  "\n\n";
+  
+  if(gameOver)
+  {
+    if(playerWon)
+    {
+      textArea.innerText += "YOU WIN!";
+    }else{
+      textArea.innerText += "DEALER WINS"
+    }
+    
+    newGameButton.style.display = "inline";
+    hitButton.style.display = "inline";
+    stayButton.style.display = "inline";
+  }
+}
 
-console.log("You're dealt: ")
-console.log(" "+getCardAsString(playerCards[0]));
-console.log(" "+getCardAsString(playerCards[1]));
+
 
 
 
